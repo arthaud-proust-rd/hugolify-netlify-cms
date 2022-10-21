0aaa7725672fed9e83020c619759b02e9a53a948
sebousan <sebousan@gmail.com>
2022-10-21 16:32:39 +0200

    image

--- collections/partials/image.js
+++ collections/partials/image.js
@@ -1,15 +1,31 @@
 export const image = {
     name: 'image',
     label: 'Image',
-    widget: 'image',
+    widget: 'object',
     required: false,
-    i18n: 'duplicate',
-    media_library: {
-        config: {
-            max_file_size: 512000
+    i18n: true,
+    field: [
+        {
+            name: 'src',
+            label: 'Image',
+            widget: 'image',
+            required: false,
+            i18n: 'duplicate',
+            media_library: {
+                config: {
+                    max_file_size: 512000
+                }
+            },
+            hint: '500Ko max, Compresser l’image avant de l’envoyer : https://tinypng.com/',
+            media_folder: '/assets/images/uploads',
+            public_folder: '/images/uploads'
+        },
+        {
+            name: 'alt',
+            label: 'Texte alternatif',
+            widget: 'string',
+            required: false,
+            i18n: true
         }
-    },
-    hint: '500Ko max, Compress image before send it: https://tinypng.com/',
-    media_folder: '/assets/images/uploads',
-    public_folder: '/images/uploads'
+    ]
 }
