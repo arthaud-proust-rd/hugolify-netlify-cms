358660e25a923db18ac9463ab10b460e1ecbc318
sebousan <sebousan@gmail.com>
2022-10-21 14:07:04 +0200

    shorcodes

---
+++ editor/twitter.js
@@ -0,0 +1,29 @@
+CMS.registerEditorComponent({
+    id: 'twitter',
+    label: 'Twitter',
+    fields: [
+        {
+            name: 'user',
+            label: 'User',
+            widget: 'string'
+        },
+        {
+            name: 'id',
+            label: 'Tweet id',
+            widget: 'string'
+        }
+    ],
+    pattern: /{{< tweet ([a-zA-Z0-9]+) >}}/,
+    fromBlock: function (match) {
+        return {
+            user: match[1],
+            id: match[2]
+        };
+    },
+    toBlock: function (obj) {
+        return `{{< tweet user="${obj.user}" id="${obj.id}" >}}`;
+    },
+    toPreview: function (obj) {
+        return `{{< tweet user="${obj.user}" id="${obj.id}" >}}`;
+    }
+});
