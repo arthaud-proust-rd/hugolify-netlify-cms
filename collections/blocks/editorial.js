3d56a4fc1291e6f7e9875ae34c42659136569401
sebousan <sebousan@gmail.com>
2022-10-24 16:28:42 +0200

    editorial

--- collections/blocks/editorial.js
+++ collections/blocks/editorial.js
@@ -15,6 +15,7 @@ export const editorial = {
         {
             name: 'direction',
             title: 'Direction',
+            widget: 'select',
             options: [
                 { label: 'texte à gauche / image à droite', value: 'ltr' },
                 { label: 'image à gauche / texte à droite', value: 'rtl' }
