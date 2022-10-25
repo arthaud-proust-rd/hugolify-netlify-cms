f540690d812ccd8efeddfb1790ae0d8293c64060
sebousan <sebousan@gmail.com>
2022-10-25 13:38:23 +0200

    admin folder app

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 // This global flag enables manual initialization.
 // import CMS, { init } from 'netlify-cms'
-const { CMS, initCMS: init } = window
+const { CMS, initCMS: init } = window;
 
-// Import all collections
-import collections from './collections/index.js';
+// Import config
+import config from './app/config.js';
 
 // Import all shortcodes for editor
-import './editor/index.js';
+import './app/editor/index.js';
 
 // Init
-init({
-    config: {
-        backend: {
-            name: 'git-gateway',
-            branch: 'main'
-        },
-
-        load_config_file: false,
-        local_backend: true,
-
-        // Internationalisation
-        locale: 'fr',
-
-        // files
-        media_folder: 'assets/images/uploads',
-        public_folder: '/images/uploads',
-
-        // slug
-        slug: {
-            encoding: 'ascii',
-            clean_accents: true,
-            sanitize_replacement: '-'
-        },
-
-        // collections
-        collections
-    }
-})
+init({ config });
