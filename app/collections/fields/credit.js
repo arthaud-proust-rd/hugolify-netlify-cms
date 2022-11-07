9a76b2b7a526eaa4da830a515d41b01b5dc18c0d
sebousan <sebousan@gmail.com>
2022-11-07 22:20:30 +0100

    fields

--- app/collections/fields/credit.js
+++ app/collections/fields/credit.js
@@ -1,7 +1,11 @@
 export const credit = {
     name: 'credit',
     label: 'Crédit',
-    widget: 'string',
+    widget: 'markdown',
+    minimal: true,
+    buttons: ["link"],
+    editor_components: [],
+    modes: ["rich_text"],
     required: false,
     i18n: true
 }
--- app/collections/fields/text-markdown.js
+++ app/collections/fields/text-markdown.js
@@ -1,7 +1,11 @@
-export const textmarkdown = {
+export const text_markdown = {
     name: 'text',
     label: 'Texte',
     widget: 'markdown',
+    minimal: true,
+    buttons: ["bold", "italic", "link"],
+    editor_components: [],
+    modes: ["rich_text"],
     required: false,
     i18n: true
 }
