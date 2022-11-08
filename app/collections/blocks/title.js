cbc76230ab86edcd2bd4a51c4dab95e4bcc45f16
sebousan <sebousan@gmail.com>
2022-11-08 15:28:34 +0100

    block title fix

--- app/collections/blocks/title.js
+++ app/collections/blocks/title.js
@@ -1,3 +1,13 @@
 import { title } from '../fields/title.js'
 
-export const block_title = title
+export const block_title = {
+    name: 'title',
+    label: 'Bloc titre',
+    widget: 'object',
+    required: false,
+    i18n: true,
+    collapsed: true,
+    fields: [
+        title
+    ]
+}
