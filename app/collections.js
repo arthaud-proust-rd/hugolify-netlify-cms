f626f401e0ae1e64f7e4c094ee238b709de8e0fc
sebousan <sebousan@gmail.com>
2022-11-15 15:41:04 +0100

    add casestudies

--- app/collections.js
+++ app/collections.js
@@ -4,6 +4,7 @@ import col_categories from './content/categories.js';
 import col_authors from './content/authors.js';
 import col_projects from './content/projects.js';
 import col_types from './content/types.js';
+// import col_casestudies from './content/casestudies.js';
 // import col_publications from './content/publications.js';
 import col_pages from './content/pages.js';
 import col_indexes from './content/indexes.js';
@@ -17,6 +18,7 @@ export const collections = [
     col_authors,
     col_projects,
     col_types,
+    // col_casestudies,
     // col_publications,
     col_indexes,
     col_pages,
