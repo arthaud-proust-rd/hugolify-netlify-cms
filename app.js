8da78fad296ef512cb9ab9ee47b3cb74fd77eacd
sebousan <sebousan@gmail.com>
2022-12-24 16:59:16 +0100

    clean

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 // import CMS, { init } from 'netlify-cms'
 const { CMS, initCMS: init } = window;
 
+// Widgets
+const parentWidget = window.NetlifyCmsWidgetParent;
+CMS.registerWidget('parent', parentWidget.control, parentWidget.preview);
+
 // Import config
 import config from './app/config.js';
 
