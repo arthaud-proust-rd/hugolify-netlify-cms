a7f880e32c7a68655812428bc6c5c1218a766da6
sebousan <sebousan@gmail.com>
2023-05-31 17:16:09 +0200

    add markdown to quote

--- admin/app/fields/quote.js
+++ admin/app/fields/quote.js
@@ -1,7 +1,11 @@
 export const quote = {
     name: 'quote',
     label: 'Citation',
-    widget: 'string',
+    widget: 'markdown',
+    minimal: true,
+    buttons: ["bold", "italic", "link"],
+    editor_components: [],
+    modes: ["rich_text"],
     required: false,
     i18n: true
 }
