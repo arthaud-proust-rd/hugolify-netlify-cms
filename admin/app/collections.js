68fb325b66090ef7ea91fec9c2b1a2e08c7e8e46
Arthaud <arthaudp33@gmail.com>
2023-08-27 18:31:03 +0200

    refactor: import collections

--- admin/app/collections.js
+++ admin/app/collections.js
@@ -1,42 +1,25 @@
-// Config
+// Hugolify CMS Config
 import config from './config.js';
 
-// Available collections
 import { availableCollections } from './settings/available-collections.js';
 
-// Collections
-let filesToImport = [];
-let selectedCollections = [];
+async function importSequentially(importsList) {
+    const collections = [];
 
-// add mandory collections
-filesToImport.push('./content/indexes.js');
-filesToImport.push('./content/pages.js');
-selectedCollections = [{},{}];
+    for (const filePath of importsList) {
+        const module = await filePath;
+        collections.push(module.default);
+    }
 
-// add selected collections
-availableCollections.forEach(element => {
-    let file = './content/' + element + '.js';
-    filesToImport.push(file);
-    selectedCollections.push({});
-});
-
-// add data
-filesToImport.push('./data/config.js');
-selectedCollections.push({});
-
-// Import and init
-const countFilesToImport = filesToImport.length;
-let i = 0;
-for (const file of filesToImport) {
-    import(file).then((module) => {
-        // add collections with order respect
-        let index = filesToImport.indexOf(file);
-        selectedCollections.splice(index, 1, module.default);
-        // Init
-        i += 1;
-        if (i === countFilesToImport) {
-            config.collections = selectedCollections;
-            window.initCMS({ config });
-        }
-    });
+    return collections;
 }
+
+importSequentially([
+    import('./content/indexes.js'),
+    import('./content/pages.js'),
+    ...availableCollections.map((element) => import(`./content/${element}.js`)),
+    import('./data/config.js')
+]).then(collections=>{
+    config.collections = collections;
+    window.initCMS({ config });
+})
