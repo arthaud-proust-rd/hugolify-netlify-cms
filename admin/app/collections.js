33552fe22a24eafb67b52c0d83116aab167c82ab
arthaud-proust <arthaudp33@gmail.com>
2023-08-29 14:10:07 +0200

    refactor: available collections

--- admin/app/collections.js
+++ admin/app/collections.js
@@ -1,25 +1,14 @@
-// Hugolify CMS Config
-import config from './config.js';
+import hugolifyConfig from './config.js';
 
-import { availableCollections } from './settings/available-collections.js';
+import indexes from "./content/indexes.js";
+import pages from "./content/pages.js";
+import availableCollections from './settings/available-collections.js';
+import config from "./data/config.js";
 
-async function importSequentially(importsList) {
-    const collections = [];
-
-    for (const filePath of importsList) {
-        const module = await filePath;
-        collections.push(module.default);
-    }
-
-    return collections;
-}
-
-importSequentially([
-    import('./content/indexes.js'),
-    import('./content/pages.js'),
-    ...availableCollections.map((element) => import(`./content/${element}.js`)),
-    import('./data/config.js')
-]).then(collections=>{
-    config.collections = collections;
-    window.initCMS({ config });
-})
+hugolifyConfig.collections = [
+    indexes,
+    pages,
+    ...availableCollections,
+    config
+];
+window.initCMS({ config: hugolifyConfig });
