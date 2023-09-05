d15b64d7c98a394cbc0105218e5b86190be1c93b
Sébastien Moulène <sebousan@gmail.com>
2023-09-05 12:25:42 +0200

    Update content.js

--- admin/app/i18n/en/content.js
+++ admin/app/i18n/en/content.js
@@ -2,20 +2,32 @@ export const content = {
     casestudies: {
         label: "Case studies",
         label_singular: "Case study",
+        description: "All case studies"
     },
     categories: {
         label: "Categories",
         label_singular: "Category",
         description: "All categories of news"
     },
+    expertises: {
+        label: "Expertises",
+        label_singular: "Expertise",
+        description: "All expertises"
+    },
     indexes: {
         label: "Indexes",
         label_singular: "Index",
         description: "Page of each index"
     },
+    offices: {
+        label: "Offices",
+        label_singular: "Office",
+        description: "All offices"
+    },
     pages: {
         label: "Pages",
         label_singular: "Page",
+        description: "All pages",
         meta: {
             parent: "Parent page"
         }
@@ -23,7 +35,12 @@ export const content = {
     persons: {
         label: "Persons",
         label_singular: "Person",
-        description: "All persons",
+        description: "All persons"
+    },
+    persons_statutes: {
+        label: "Statuts",
+        label_singular: "Statut",
+        description: "All status of persons"
     },
     posts: {
         label: "News",
@@ -32,6 +49,7 @@ export const content = {
     projects: {
         label: "Projects",
         label_singular: "Project",
+        description: "All projects"
     },
     projects_tags: {
         label: "Tags of project",
@@ -46,6 +64,12 @@ export const content = {
     publications: {
         label: "Publications",
         label_singular: "Publication",
+        description: "All publications"
+    },
+    publications_tags: {
+        label: "Tags",
+        label_singular: "Tag",
+        description: "All tags of publications"
     },
     tags: {
         label: "Tags",
